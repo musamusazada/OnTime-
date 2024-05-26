ec99826e00c8976bc7f2d2f41ee082e81d9d0b19
Musa Musazada <musa.m@fireart-d.com>
2024-05-26 19:19:59 +0200

    feat: init setup, folder structure, movement component

---
+++ components/movement/movement.gd
@@ -0,0 +1,16 @@
+class_name Movement 
+extends Node
+
+@export var actor: CharacterBody2D
+@export var min_slide_angle = 0.0
+
+
+
+func _ready():
+	actor.wall_min_slide_angle = min_slide_angle
+	actor.motion_mode = actor.MOTION_MODE_FLOATING
+
+func _physics_process(delta: float) -> void:
+	actor.move_and_slide()
+
+
---
+++ components/movement/movement_stats.gd
@@ -0,0 +1,6 @@
+class_name MovementStats extends Resource
+
+
+@export var max_speed = 100.0 
+@export var acceleration = 1000.0
+@export var friction = 500.0
